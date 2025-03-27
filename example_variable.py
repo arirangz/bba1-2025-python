ae593eb42e5e750bca76aebdc01d21f689eec312
Anthony L <laplane_anthony@yahoo.fr>
2025-03-27 09:00:23 +0000

    ex2

---
+++ example_variable.py
@@ -0,0 +1,6 @@
+age = 10
+
+first_name = "John"
+
+print(age, first_name)
+
---
+++ exercise_hello2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello {first_name}. You are {age} years old.")
