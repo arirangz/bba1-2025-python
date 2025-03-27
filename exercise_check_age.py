321de2e468e5892347adcb710d2922b7110d2d0c
Anthony L <laplane_anthony@yahoo.fr>
2025-03-27 10:02:42 +0000

    exercise check age

---
+++ exercise_check_age.py
@@ -0,0 +1,7 @@
+username = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if (age < 18):
+    print("Our age is not suitable for your age")
+else:
+    print(f"Welcome to our website {username}")
