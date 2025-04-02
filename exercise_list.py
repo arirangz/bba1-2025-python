dd11ac4ed827e7f932503e32850675de1db085e8
Anthony L <laplane_anthony@yahoo.fr>
2025-04-02 10:12:36 +0000

    exercise list

---
+++ exercise_list.py
@@ -0,0 +1,10 @@
+countries = ["Nepal", "France", "China"]
+
+print(f"second country is {countries[1]}")
+
+countries.append("India")
+
+countries.pop(0)
+
+for country in countries:
+    print(country)
