35c50c3103ac684bef5066b5bb46e851d8b4cf5c
Anthony L <laplane_anthony@yahoo.fr>
2025-04-02 07:57:28 +0000

    while true

---
+++ example_while_true.py
@@ -0,0 +1,7 @@
+number = 0
+
+while True:
+    number += 1
+    print(f"hello {number}")
+    if number == 10:
+        break
---
+++ example_while_true2.py
@@ -0,0 +1,10 @@
+while True:
+    user_input = input("Enter 'quit' to exit: ")
+   
+    if user_input == 'quit':
+        print("Exiting the program.")
+        break
+   
+    print("You entered:", user_input)
+
+
