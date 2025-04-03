3f82773d91406bb74d5e1fd1b412c43ba062a54a
Anthony L <laplane_anthony@yahoo.fr>
2025-04-03 09:53:38 +0000

    ex dictio

---
+++ exercise_dictionnary.py
@@ -0,0 +1,9 @@
+student = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "age": 28
+}
+
+print("Student first name: ", student["first_name"])
+print(f"Student last name: {student["last_name"]}")
+print(f"Student age: {student["age"]}")
