5e4e8038e4847b9857823867de94d2f952c2f86c
Anthony L <laplane_anthony@yahoo.fr>
2025-04-09 10:05:18 +0000

    exercise function

---
+++ exercise_function.py
@@ -0,0 +1,9 @@
+def display_full_name(first_name, last_name):
+    print(first_name, last_name)
+
+first_name = "John"
+last_name = "Doe"
+
+display_full_name(first_name, last_name)
+
+display_full_name("Jane", "Spencer")
