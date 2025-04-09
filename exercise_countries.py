ae2bbd8ede6e5984608c93b183ad84f556d91780
Anthony L <laplane_anthony@yahoo.fr>
2025-04-09 08:19:16 +0000

    countries

---
+++ exercise_countries.py
@@ -0,0 +1,18 @@
+countries = [
+    {"name": "Japan", "population": 124_000_000},
+    {"name": "Nepal", "population": 29_000_000},
+    {"name": "India", "population": 1_438_000_000},
+    {"name": "Sri Lanka", "population": 22_000_000},
+    {"name": "France", "population": 68_000_000},
+]
+biggest_country = countries[0]
+for country in countries:
+    print(f"Name: {country["name"]} - Population: {country["population"]:,}")
+    # Add a if to compare country with biggest_country
+    # if true we store the country in biggest_country
+    if country["population"] > biggest_country["population"]:
+        biggest_country = country
+    
+
+# After the loop, we display the biggest country
+print(f"The biggest country is {biggest_country["name"]} with a population of {biggest_country["population"]:,}")
